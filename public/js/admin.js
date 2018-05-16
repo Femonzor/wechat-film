b0a09a1c866a898e2cc461cae476c5f9ae7fe7a7
Femonzor <wayneyang3@gmail.com>
2018-05-16 22:43:27 +0800

    delete

---
+++ public/js/admin.js
@@ -0,0 +1,15 @@
+$(function () {
+    $(".delete").on("click", function (event) {
+        var $target = $(event.target);
+        var id = $target.data("id");
+        var $tr = $(".item-id-" + id);
+        $.ajax({
+            type: "DELETE",
+            url: "/admin/list?id=" + id,
+        }).done(function (results) {
+            if (results.success === 1) {
+                if ($tr.length > 0)  $tr.remove();
+            }
+        });
+    });
+});
--- src/app2.js
+++ src/app2.js
@@ -15,7 +15,7 @@ app.engine("art", expressArtTemplate);
 app.set("view engine", "art");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static("bower_components"));
+app.use(express.static("public"));
 app.locals.moment = moment;
 app.listen(port);
 
@@ -29,34 +29,6 @@ app.get("/", (request, response) => {
             movies
         })
     });
-    // response.render("pages/index", {
-    //     title: "imooc",
-    //     movies: [{
-    //         title: "机械战警",
-    //         id: 1,
-    //         poster: "https://femonzor.com/resource/images/koala.jpg"
-    //     }, {
-    //         title: "机械战警",
-    //         id: 2,
-    //         poster: "https://femonzor.com/resource/images/koala.jpg"
-    //     }, {
-    //         title: "机械战警",
-    //         id: 3,
-    //         poster: "https://femonzor.com/resource/images/koala.jpg"
-    //     }, {
-    //         title: "机械战警",
-    //         id: 4,
-    //         poster: "https://femonzor.com/resource/images/koala.jpg"
-    //     }, {
-    //         title: "机械战警",
-    //         id: 5,
-    //         poster: "https://femonzor.com/resource/images/koala.jpg"
-    //     }, {
-    //         title: "机械战警",
-    //         id: 6,
-    //         poster: "https://femonzor.com/resource/images/koala.jpg"
-    //     }]
-    // });
 });
 
 app.get("/movie/:id", (request, response) => {
@@ -67,19 +39,6 @@ app.get("/movie/:id", (request, response) => {
             movie
         });
     });
-    // response.render("pages/detail", {
-    //     title: "imooc详细页",
-    //     movie: {
-    //         director: "帕迪利亚",
-    //         country: "美国",
-    //         title: "机械战警",
-    //         year: 2014,
-    //         poster: "https://femonzor.com/resource/images/koala.jpg",
-    //         language: "英语",
-    //         flash: "http://img-hws.y8.com/cloud/y8-rollover/videos/55953/785b538bd4db8ad9b5dd45e6950e1cb2e4135abb.swf",
-    //         summary: "哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈"
-    //     }
-    // });
 });
 
 app.get("/admin/movie", (request, response) => {
@@ -142,18 +101,17 @@ app.get("/admin/list", (request, response) => {
             movies
         })
     });
-    // response.render("pages/list", {
-    //     title: "imooc列表页",
-    //     movies: [{
-    //         title: "机械战警",
-    //         id: 1,
-    //         director: "帕迪利亚",
-    //         country: "美国",
-    //         year: 2014,
-    //         poster: "https://femonzor.com/resource/images/koala.jpg",
-    //         language: "英语",
-    //         flash: "http://img-hws.y8.com/cloud/y8-rollover/videos/55953/785b538bd4db8ad9b5dd45e6950e1cb2e4135abb.swf",
-    //         summary: "哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈"
-    //     }]
-    // });
+});
+
+app.delete("/admin/list", (request, response) => {
+    const id = request.query.id;
+    if (id) {
+        Movie.remove({ _id: id }, (error, movie) => {
+            if (error) {
+                console.log(error);
+            } else {
+                response.json({ success: 1 });
+            }
+        });
+    }
 });
